39e0a975b9c0bd50944f7142f2c91983becf72f6
Egzamin <krmlqq@gmail.com>
2023-05-18 12:32:26 +0200

    insert text
    .

--- advert_portal/src/App.js
+++ advert_portal/src/App.js
@@ -26,12 +26,30 @@ function MainDescription({jobPosition, home, desc})
     return description;
 }
 
+function ContactDetails({email, phoneNumber, dateOfBirth})
+{
+    const details = <>
+        <p>E-mail: {email}</p>
+        <p>Number telefonu: {phoneNumber}</p>
+        <p>Data Urodzenia: {dateOfBirth}</p>
+    </>
+
+    return details;
+}
+
+function Experience({text})
+{
+    return <p>{text}</p>;
+}
+
 function App()
 {
     const content = <>
     <Logo source={"logo2.png"} altText={"test"}/>
     <Name firstName={"Krzysztof"} lastName={"Malec"}/>
     <MainDescription jobPosition={"Programista"} home={"Limanowa"} desc={"Młody programista"}/>
+    <ContactDetails email={"asd@gmail.com"} phoneNumber={"123456789"} dateOfBirth={"12.12.1912"}/>
+    <Experience text={"Mam duże doświadczenie"}/>
 </>
 
     return content;
